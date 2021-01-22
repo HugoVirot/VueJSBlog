673dde46a57e032a46a302cd417e5dd749b3a8eb
HugoVirot <hugo.virot@gmail.com>
2021-01-22 11:40:50 +0100

    premier commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div v-for="unicorn in unicorns" :key="unicorn._id">
+    <p>{{ unicorn.name }}</p>
+    <p>{{ unicorn.age }}</p>
+    <p>{{ unicorn.colour }}</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      unicorns: [],
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    getData(component) {
+      axios
+        .get(
+          "https://crudcrud.com/api/addf3192190444b6a72df85cc11f0dd2/unicorns"
+        )
+        .then(function (response) {
+          console.log("licornes récupérées")
+          component.unicorns = response.data;
+          console.log(response.data);
+        })
+        .catch(console.log("échec requête"));
+    },
+    sendData() {
+      var licorne = {
+        name: "machin",
+        age: 3,
+        colour: "black",
+      };
+      axios
+        .post(
+          "https://crudcrud.com/api/addf3192190444b6a72df85cc11f0dd2/unicorns",
+          licorne
+        )
+        .then(() => {
+          console.log("licorne créée");
+        })
+        .catch(console.log("échec création"));
+    },
+  },
+  created() {
+    this.getData(this);
+    this.sendData();
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
